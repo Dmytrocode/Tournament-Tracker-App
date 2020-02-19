2a4e3f77d756b6cb5a1b1a4a5f628e267b8263f3
Dmytro Kavetskyy <dmytrocontact@gmail.com>
2020-02-18 20:37:24 -0600

    Setup for the Class elements

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        ///Represents one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents score for particular team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents matchup this team came from as winner
+        /// 
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
-    public class Class1
+    public class TeamModel
     {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
+        public string TeamName { get; set; }
+
     }
 }
