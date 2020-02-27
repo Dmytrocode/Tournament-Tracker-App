d079dd40ca375703fac65449f3f6efbfd634141b
Dmytro Kavetskyy <dmytrocontact@gmail.com>
2020-02-26 18:16:26 -0600

    Started an Interface and Class to Setup the connection between the program and the SQL / Textfile database

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if(database)
+            {
+                //TODO create SQL connection
+            }
+
+            if(textFiles)
+            {
+                //TODO Create Text Connection
+            }
+            
+        }
+
+    }
+}
