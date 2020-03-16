7d8c8187820ade410ef86368ad19b3b9d8429a0b
Dmytro Kavetskyy <dmytrocontact@gmail.com>
2020-03-16 01:32:11 -0600

    Add file settings

--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess.TextConnector
 {
     public static class TextConnectorProcessor
     {
-        public static string FullFilePath(string fileName)
+        public static string FullFilePath(this string fileName) //PrizeModel.csv
         {
-            return $"{ ConfigurationManager.AppSettings["filePath"] } \\";
+            // C:\data\TournamentTracker [ \ PrizeModel.csv ]
+            return $"{ ConfigurationManager.AppSettings["filePath"] } \\{ fileName }";
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if(!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModel(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>;
+
+            foreach(string line in lines)
+            {
+
+            }
+
+            return List<PrizeModel>
         }
     }
 }
